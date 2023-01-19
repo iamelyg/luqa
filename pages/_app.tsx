1509a9b3b18ca1cc0462889fc1c0f43e05758800
iamelyg <ely.gon771@gmail.com>
2023-01-19 18:11:50 -0500

    container element config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, Container } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 
 import theme from '@/theme';
@@ -6,7 +6,14 @@ import theme from '@/theme';
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <Container
+      backgroundColor='white'
+      boxShadow='md'
+      marginY={4}
+      padding={4}
+      maxWidth='container.xl'>
+        <Component {...pageProps} />
+      </Container>
     </ChakraProvider>
   )
 }
