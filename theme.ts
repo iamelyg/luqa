b9447decd215a3c2c5feb35dcf0906b35bc38019
iamelyg <ely.gon771@gmail.com>
2023-01-19 18:08:19 -0500

    styles global

--- theme.ts
+++ theme.ts
@@ -3,5 +3,12 @@ import { extendTheme, theme } from "@chakra-ui/react";
 export default extendTheme({
     colors: {
         primary: theme.colors['purple'],
+    },
+    styles: {
+        global: {
+            body: {
+                backgroundColor: 'primary.50'
+            }
+        }
     }
 })
