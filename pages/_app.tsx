29a1a31990766a3409a1560d6d470fd1f4463bb3
iamelyg <ely.gon771@gmail.com>
2023-01-26 18:21:35 -0800

    clear

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
-import { ChakraProvider, Container, Heading, Image, VStack } from '@chakra-ui/react';
+import { ChakraProvider, Container } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 
 import DesktopNavbar from '@/components/navbar/DesktopNavbar';
 import MobileDrawer from '@/components/navbar/Mobile';
 
 import theme from '@/theme';
-import { INFORMATION } from '@/app/constants';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
