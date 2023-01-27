f220f97b056524d867c6bf23feac92d97e165865
iamelyg <ely.gon771@gmail.com>
2023-01-26 18:19:25 -0800

    constants

--- app/constants.ts
+++ app/constants.ts
@@ -1,14 +1,35 @@
-export const INFORMATION = {
-    logo: 'https://via.placeholder.com/150',
-    banner: '',
-    title: 'Luqa',
-    description: 'Tienda de auriculares calidad precio',
-    phone: '987654321',
-    sheet: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRSziis5p9IykazYt8zOeEjTyKLYXgma0fu-BhrWG4TVqVRhq3OSfd6BYJR4K76aY60BP9PY5qYZeYu/pub?output=csv',
-    color: 'pink',
-    menu: [
-        { label: 'Inicio', slug: '/', icon: 'fa-home' },
-        { label: 'Tienda', slug: '/tienda', icon: 'fa-shop' },
-        { label: 'Atención al cliente', slug: '/atencion-cliente', icon: 'fa-headset' },
-    ]
+export const INFORMATION: Infomation = {
+  logo: "https://via.placeholder.com/150",
+  banner: "",
+  title: "Luqa",
+  description: "Tienda de auriculares calidad precio",
+  phone: "987654321",
+  sheet:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRSziis5p9IykazYt8zOeEjTyKLYXgma0fu-BhrWG4TVqVRhq3OSfd6BYJR4K76aY60BP9PY5qYZeYu/pub?output=csv",
+  color: "#0059f7",
+  menu: [
+    { label: "Inicio", slug: "/", icon: "fa-home" },
+    { label: "Tienda", slug: "/tienda", icon: "fa-shop" },
+    {
+      label: "Atención al cliente",
+      slug: "/atencion-cliente",
+      icon: "fa-headset",
+    },
+  ],
 };
+
+interface Infomation {
+  logo: string;
+  banner: string;
+  title: string;
+  description: string;
+  phone: string;
+  sheet: string;
+  color: string;
+  menu: Menu[];
+}
+interface Menu {
+  label: string;
+  slug: string;
+  icon: string;
+}
--- theme.ts
+++ theme.ts
@@ -4,15 +4,17 @@ import { INFORMATION } from "./app/constants";
 
 export default extendTheme({
     colors: {
-        primary: theme.colors[INFORMATION.color],
         primary: {
             10: 'rgba(1, 102, 253, .1)',
-        }
+            100: theme.colors[INFORMATION.color],
+        },
+        bgBody: '#070e27',
     },
     styles: {
         global: {
             body: {
-                backgroundColor: 'primary.10'
+                backgroundColor: 'bgBody',
+                color: 'white'
             }
         }
     }
