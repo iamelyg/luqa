babad0af8d87939e2e22731e28962252362b2817
iamelyg <ely.gon771@gmail.com>
2023-01-31 18:18:54 -0500

    clear

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,8 @@
 import { Grid } from "@chakra-ui/react";
-import { ITodo } from "./tienda/context/types";
-import { useTodoContext } from "./tienda/StoreProvider";
 
 const IndexRoute: React.FC = () => {
-  const { todos } = useTodoContext();
   return <Grid>
     <p>hola inciio</p>
-    {todos.map((todo: ITodo) => <p key={todo.id}>{todo.title}</p>)}
   </Grid>;
 }
 
