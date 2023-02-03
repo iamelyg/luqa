a0d1deb5563f102c08ae7d8af9e71e16f60965d9
iamelyg <ely.gon771@gmail.com>
2023-02-03 12:47:33 -0500

    width modal - quiek view button

--- pages/tienda/components/QuickView.tsx
+++ pages/tienda/components/QuickView.tsx
@@ -11,14 +11,14 @@ const QuickView: React.FC<Props> = ({ selectedProduct }) => {
 
     return (
         <>
-            <Button onClick={onOpen}>Open Modal</Button>
+            <Button onClick={onOpen}>Vista rápida</Button>
             <Modal size='xl' colorScheme='brand' scrollBehavior='inside' blockScrollOnMount={true} isOpen={isOpen} onClose={onClose} isCentered motionPreset='slideInBottom'>
                 <ModalOverlay backdropFilter='blur(5px)' />
                 <ModalContent w={{ base: '90%', md: '80%' }} maxWidth='container.xl'>
                     <ModalHeader>{selectedProduct.title}</ModalHeader>
                     <ModalCloseButton />
-                    <ModalBody>
-                        <Image
+                    <ModalBody flexDirection={{ base: 'column', md: 'row' }} display='flex'>
+                        <Image 
                             alt={selectedProduct.title} src={selectedProduct.image} objectFit='cover' borderRadius={10} />
                         <Text >{selectedProduct.description}</Text>
                         <Text fontWeight='bold'>{selectedProduct.price}</Text>
