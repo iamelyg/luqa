0d963f5068a070f60126b268ba5bebcb91a1a3e6
iamelyg <ely.gon771@gmail.com>
2023-02-06 23:29:44 -0500

    input by form

--- pages/carrito-de-compra/index.tsx
+++ pages/carrito-de-compra/index.tsx
@@ -1,5 +1,41 @@
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Input, Card, Text } from '@chakra-ui/react';
 
 const ShoppingCart: React.FC = () => {
-    return <div>carrito de compra</div>
+    return <Tabs>
+        <TabList>
+            <Tab>One</Tab>
+            <Tab isDisabled>Two</Tab>
+            <Tab>Three</Tab>
+        </TabList>
+        <TabPanels>
+            <TabPanel>
+                <Card>
+                   {
+                    inputForm.map((inp, id) => <Input key={id} placeholder={inp.placeholder}/>)
+                   }
+                </Card>
+            </TabPanel>
+            <TabPanel>2</TabPanel>
+            <TabPanel>3</TabPanel>
+        </TabPanels>
+    </Tabs>
 }
+
+const inputForm = [{
+    label: 'Nombres',
+    type: 'text',
+    placeholder: 'Ingresa tu nombre'
+}, {
+    label: 'Apellidos',
+    type: 'text',
+    placeholder: 'Ingresa tus apellidos'
+}, {
+    label: 'Correo',
+    type: 'email',
+    placeholder: 'Ingresa tu correo'
+}, {
+    label: 'Celular',
+    type: 'number',
+    placeholder: 'Ingresa tu n° celular'
+}]
 export default ShoppingCart;
