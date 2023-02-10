84f49b96c5968ccec94e40f6dbed69b1e6771060
iamelyg <ely.gon771@gmail.com>
2023-02-10 17:18:55 -0500

    get email

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Image, Flex, Button, HStack, chakra, Icon, Text, VStack, ListItem, ListIcon, UnorderedList, Stack, Container } from '@chakra-ui/react';
+import { Image, Flex, Button, HStack, chakra, Icon, Text, VStack, ListItem, IconButton, UnorderedList, Stack, Container, Input } from '@chakra-ui/react';
 import Logo from '../../public/logo-luqa-pe.png';
 import Link from 'next/link';
-import { BsMoonStars, BsSun } from 'react-icons/bs';
+import { FaPaperPlane } from 'react-icons/fa';
 
 import { DrawerExample } from '@/pages/tienda/components/Cart';
 
@@ -12,11 +12,22 @@ const Footer: React.FC = () => {
     return (
         <chakra.footer bg='brand.800' color='whiteAlpha.900' >
             <Container max-width='container.xl' padding={10}>
-                <HStack justifyContent='baseline' gap={8}>
+                <Text>Sé el primero en conocer lo nuevo de Luqa</Text>
+                <HStack>
+                    <Input
+                        placeholder="Ingresa tu correo"
+                        size="md"
+                        type="email"
+                    />
+                    <IconButton aria-label='Search database' colorScheme='brand' icon={<Icon as={FaPaperPlane} w={6} h={6} />} />
+                </HStack>
+
+
+                <HStack justifyContent='baseline' gap={8} alignItems='start'>
                     {INFORMATION.footer.map((menu) => <VStack key={menu.title} >
                         <Text>{menu.title}</Text>
                         <UnorderedList listStyleType='none'>
-                            {menu.submenu.map(sub => <ListItem><Link href={sub.slug}>{sub.label}</Link></ListItem>)}
+                            {menu.submenu.map(sub => <ListItem key={sub.slug}><Link href={sub.slug}>{sub.label}</Link></ListItem>)}
                         </UnorderedList>
                     </VStack>)}
                     <Stack>
