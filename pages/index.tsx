9e9277dacba54a1e2a0b1ae5ca6c02e7ee4a4948
iamelyg <ely.gon771@gmail.com>
2023-02-24 19:51:33 -0500

    carousel for home0

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,23 +4,23 @@ import { Carousel } from 'react-responsive-carousel';
 
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-import fongex2pro from '../src/images/home/auricular-hifi-fonge-x2pro-m.png';
-import fongex2proD from '../src/images/home/auricular-hi-fi-fonge-x2-pro.png';
+import fongex2proM from '../src/images/home/auricular-hifi-fonge-x2pro-m.png';
+import fongex2pro from '../src/images/home/auricular-hi-fi-fonge-x2-pro.png';
+import soundpeatsM from '../src/images/home/audifonos-soundpeats-m.png';
 import soundpeats from '../src/images/home/audifonos-soundpeats.png';
+import air3proM from '../src/images/home/audifono-air3-pro-m.png';
 import air3pro from '../src/images/home/audifono-air3-pro.png';
 
 const IndexRoute: React.FC = () => {
-  const [isLargerThan800] = useMediaQuery('(min-width: 800px)', { ssr: true, fallback: false });
-
   return <>
-    <Image style={{ borderRadius: '1.5rem' }}
+    {/* <Image style={{ borderRadius: '1.5rem' }}
       src={isLargerThan800 ? fongex2proD : fongex2pro}
       alt="auricular hifi Fonge x2 pro "
     // width={500} automatically provided
     // height={500} automatically provided
     // blurDataURL="data:..." automatically provided
     // placeholder="blur" // Optional blur-up while loading
-    />
+    /> */}
     <MainCarousel />
   </>;
 }
@@ -28,30 +28,24 @@ const IndexRoute: React.FC = () => {
 export default IndexRoute;
 
 const MainCarousel = () => {
-  const [isLargerThan800] = useMediaQuery('(min-width: 800px)', { ssr: true, fallback: false });
+  const [isLargerThan800] = useMediaQuery('(min-width: 600px)', { ssr: true, fallback: false });
 
-  return <Carousel>
-    <div>
-      <Image style={{ borderRadius: '1.5rem' }}
-        src={isLargerThan800 ? fongex2proD : fongex2pro}
-        alt="auricular hifi Fonge x2 pro "
-      // width={500} automatically provided
-      // height={500} automatically provided
-      // blurDataURL="data:..." automatically provided
-      // placeholder="blur" // Optional blur-up while loading
-      />
-    </div>
-    <div>
-      <Image
-        src={soundpeats}
-        alt="auricular hifi Fonge x2 pro "
-      />
-    </div>
-    <div>
-      <Image
-        src={air3pro}
-        alt="auricular hifi Fonge x2 pro "
-      />
-    </div>
+  return <Carousel autoPlay={true} showArrows={false} showStatus={false} interval={3000} infiniteLoop>
+    <Image style={{ borderRadius: '1.5rem', aspectRatio: isLargerThan800 ? '16/9' : '9/16' }}
+      src={isLargerThan800 ? fongex2pro : fongex2proM}
+      alt="auricular hifi Fonge x2 pro "
+    // width={500} automatically provided
+    // height={500} automatically provided
+    // blurDataURL="data:..." automatically provided
+    // placeholder="blur" // Optional blur-up while loading
+    />
+    <Image style={{ borderRadius: '1.5rem', aspectRatio: isLargerThan800 ? '16/9' : '9/16' }}
+      src={isLargerThan800 ? soundpeats : soundpeatsM}
+      alt="auricular hifi Fonge x2 pro "
+    />
+    <Image style={{ borderRadius: '1.5rem', aspectRatio: isLargerThan800 ? '16/9' : '9/16' }}
+      src={isLargerThan800 ? air3pro : air3proM}
+      alt="auricular hifi Fonge x2 pro "
+    />
   </Carousel>
 }
