366351825ca7f1f8ddb9f30d97323944a8e97193
iamelyg <ely.gon771@gmail.com>
2023-02-25 20:53:59 -0500

    utilities sheetstojson

--- api/product.js
+++ api/product.js
@@ -1,34 +1,36 @@
 import { google } from 'googleapis';
 
+import { sheetsToJson } from '@/src/utils/utilities';
+
 export async function getProductList() {
-    try {
-        const auth = await google.auth.getClient({ scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'] });
+	try {
+		const auth = await google.auth.getClient({ scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'] });
 
-        const sheets = google.sheets({ version: 'v4', auth });
+		const sheets = google.sheets({ version: 'v4', auth });
 
-        const range = `stock!A1:I5`;
+		const range = `stock!A1:I5`;
 
-        const response = await sheets.spreadsheets.values.get({
-            spreadsheetId: process.env.SHEET_ID,
-            range,
-        });
+		const response = await sheets.spreadsheets.values.get({
+			spreadsheetId: process.env.SHEET_ID,
+			range,
+		});
 
-        const data = sheetsToJson(response.data.values).map(product => ({
-            ...product,
-            price: Number(product.price),
-            regularPrice: Number(product.regularPrice)
-        }));
-        console.log('sheetsToJson', data)
-        return {
-            success: true,
-            data,
-        }
-    } catch (e) {
-        return {
-            success: false, message: 'No se puede acceder a esta info.'
-        }
-    }
+		const data = sheetsToJson(response.data.values).map(product => ({
+			...product,
+			price: Number(product.price),
+			regularPrice: Number(product.regularPrice)
+		}));
+		console.log('sheetsToJson', data)
+		return {
+			success: true,
+			data,
+		}
+	} catch (e) {
+		return {
+			success: false, message: 'No se puede acceder a esta info.'
+		}
+	}
 }
 
 // const sheetsToJson = ([keys, ...values]) => values.map(vs => vs.reduce((acc, v, i) => (acc[keys[i]] = v, acc), {}))
-const sheetsToJson = ([keys, ...values]) => values.map(vs => Object.fromEntries(vs.map((v, i) => [keys[i], v.includes('|') ? v.split('|') : v])))
+// const sheetsToJson = ([keys, ...values]) => values.map(vs => Object.fromEntries(vs.map((v, i) => [keys[i], v.includes('|') ? v.split('|') : v])))
--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -1,3 +1,5 @@
 const parseCurrency = (value: number): String => value.toLocaleString('es-PE', { style: 'currency', currency: 'PEN' })
 
-export { parseCurrency }
+const sheetsToJson = ([keys, ...values]: string[][]): Array<{}> => values.map(vs => Object.fromEntries(vs.map((v, i) => [keys[i], v.includes('|') ? v.split('|') : v])))
+
+export { parseCurrency, sheetsToJson }
