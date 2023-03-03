30410c1eba2d29e5fb2ee98610ac5c9563d0a9f6
iamelyg <ely.gon771@gmail.com>
2023-03-03 13:45:59 -0500

    show footer fixed on mobile

--- src/components/tienda/components/QuickView.tsx
+++ src/components/tienda/components/QuickView.tsx
@@ -1,8 +1,7 @@
-import { useDisclosure, Button, Icon, Center, Text, Grid, Image, VStack, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, HStack } from '@chakra-ui/react';
+import { useDisclosure, Button, Icon, Center, Text, Grid, Image, VStack, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, HStack, useMediaQuery } from '@chakra-ui/react';
 import { BsEye } from "react-icons/bs";
 import parse from 'html-react-parser';
 import { Carousel } from 'react-responsive-carousel';
-// import Image from 'next/image';
 
 import { Product } from '@/src/product/types';
 import { parseCurrency } from '@/src/utils/utilities';
@@ -14,20 +13,21 @@ interface Props {
 
 const QuickView: React.FC<Props> = ({ selectedProduct }) => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
+	const [isLargerThan800] = useMediaQuery('(min-width: 700px)')
 
 	return (
 		<>
 			<Button onClick={onOpen} variant='ghost' position='absolute' h={10} w={10} bottom={1} right={1}><Icon w={5} h={5} as={BsEye} /></Button>
 			<Modal size='xl' colorScheme='brand' scrollBehavior='inside' blockScrollOnMount={true} isOpen={isOpen} onClose={onClose} isCentered motionPreset='slideInBottom'>
 				<ModalOverlay backdropFilter='blur(5px)' />
-				<ModalContent w={{ base: '90%', md: '80%' }} maxWidth='container.xl' maxH='95%'>
+				<ModalContent w={{ base: '90%', lg: '80%' }} maxWidth='container.xl' maxH='95%'>
 					<ModalCloseButton />
 					<ModalBody >
 						<Grid gridGap={6} templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}>
 							{Array.isArray(selectedProduct.images)
-								&& <Carousel showStatus={false} showThumbs={true} infiniteLoop={true}>
+								&& <Carousel showStatus={false} showThumbs={isLargerThan800} infiniteLoop={true} thumbWidth={50}>
 									{selectedProduct.images.map(((url, id) => <img
-										alt={selectedProduct.title} src={url} key={id} />
+										alt={selectedProduct.title} src={url} key={id} style={{ maxWidth: '300px'}} />
 									))}
 									{/* {selectedProduct.images.map(((url, id) => <Image
 										alt={selectedProduct.title} src={url} objectFit='cover' borderRadius={10} key={id} />
@@ -44,13 +44,17 @@ const QuickView: React.FC<Props> = ({ selectedProduct }) => {
 									<Text>Regular</Text>
 									<Text as='del'>{parseCurrency(selectedProduct.regularPrice)}</Text>
 								</HStack>
+								{isLargerThan800 && <ModalFooter>
+									<AddToCartButton product={selectedProduct} />
+									<Button variant='ghost'>Comprar ahora</Button>
+								</ModalFooter>}
 							</VStack>
 						</Grid>
 					</ModalBody>
-					<ModalFooter>
-						<AddToCartButton product={selectedProduct} />
-						<Button variant='ghost'>Comprar ahora</Button>
-					</ModalFooter>
+					{!isLargerThan800 && <ModalFooter>
+									<AddToCartButton product={selectedProduct} />
+									<Button variant='ghost'>Comprar ahora</Button>
+								</ModalFooter>}
 				</ModalContent>
 			</Modal>
 		</>
