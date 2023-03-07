1dca1c5c0208f7a5727b934899a04d83e32f1f1e
iamelyg <ely.gon771@gmail.com>
2023-03-06 20:06:21 -0500

    add to one product

--- src/components/tienda/context/store.context.d.tsx
+++ src/components/tienda/context/store.context.d.tsx
@@ -13,7 +13,11 @@ const StoreProvider: React.FC<Props> = ({ children }) => {
     const [cart, setCart] = useState<ProductInCart[]>([]);
 
     const addToCart = (product: Product) => {
-        setCart(state => state.concat({ item: product, quantity: 1 }))
+        if(cart.find(prod => prod.item.id === product.id)) {
+            addProduct(product.id)
+        } else {
+            setCart(state => state.concat({ item: product, quantity: 1 }))
+        }
     }
 
     const removeFromCart = (id: string) => {
