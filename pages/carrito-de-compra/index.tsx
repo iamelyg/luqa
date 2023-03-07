46a092e4fd7a7afb54d4fe76b7b27cf6f945300c
iamelyg <ely.gon771@gmail.com>
2023-03-06 21:39:02 -0500

    tab active

--- pages/carrito-de-compra/index.tsx
+++ pages/carrito-de-compra/index.tsx
@@ -1,13 +1,22 @@
-import { Tabs, TabList, TabPanels, Tab, TabPanel, Input, Card, Text, VStack, Box } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Input, Text, Box, Button } from '@chakra-ui/react';
 
 import { INFORMATION } from '@/src/utils/constants';
 
 const ShoppingCart: React.FC = () => {
-    return <Tabs bg='white' w='container.md' m='auto' shadow='lg' p={6}>
+    const [tabIndex, setTabIndex] = useState<number>(0);
+    const [activeTabs, setActiveTabs] = useState<number[]>([0]);
+
+    const onClickNext = (index: number) => {
+        setTabIndex(index);
+        setActiveTabs([...activeTabs, index]);
+    }
+
+    return <Tabs index={tabIndex} onChange={index => setTabIndex(index)} bg='white' w='container.md' m='auto' shadow='lg' p={6}>
         <TabList>
             <Tab>Identificación</Tab>
-            <Tab>Delivery</Tab>
-            <Tab isDisabled>Three</Tab>
+            <Tab isDisabled={!Boolean(activeTabs.find(tab => tab === 1))}>Delivery</Tab>
+            <Tab isDisabled={!Boolean(activeTabs.find(tab => tab === 2))}>Pago</Tab>
         </TabList>
         <TabPanels>
             <TabPanel>
@@ -17,14 +26,16 @@ const ShoppingCart: React.FC = () => {
                         <Input placeholder={inp.placeholder} />
                     </Box>)
                 }
+                <Button onClick={() => onClickNext(1)}>Siguiente</Button>
             </TabPanel>
             <TabPanel>
-            {
+                {
                     INFORMATION.cart.location.map((inp, id) => <Box key={id} marginY={5}>
                         <Text mb='8px'>{inp.label}</Text>
                         <Input placeholder={inp.placeholder} />
                     </Box>)
                 }
+                <Button onClick={() => onClickNext(2)}>Siguiente</Button>
             </TabPanel>
             <TabPanel>3</TabPanel>
         </TabPanels>
